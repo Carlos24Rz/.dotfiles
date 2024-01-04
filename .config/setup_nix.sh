cd3487fa1c0ce4ab6b4fcde99c2f5ecd38df373d
Carlos24Rz <myrandom24things@gmail.com>
2024-01-04 12:39:31 -0600

    Adds utilities to track installed nix packages

--- .config/setup_nix.sh
+++ .config/setup_nix.sh
@@ -1,24 +1,43 @@
-# Install a packaga
-nixinstall()
+# Setup a new machine
+function nixsetup()
 {
-	nix-env -iA nixpkgs."$1";
+	echo "Installing nix..."
+	sh <(curl -L https://nixos.org/nix/install) --daemon;
+	source /etc/profile;
+
+	echo "Installing packages"
+        if [ -f ~/.config/nix_packages.yaml ]; then
+		cat ~/.config/nix_packages.yaml | sed -n 's/^-\s\(.*\)$/\1/p' | xargs -n 1 nix-env -iA;
+	fi
+}
+
+# Install a package
+function nixinstall()
+{
+	if nix-env -iA nixpkgs."$1" && ! grep "^- $1$" ~/.config/nix_packages.yaml > /dev/null 2>&1; then
+		cat >> ~/.config/nix_packages.yaml <<EOF
+- $1
+EOF
+        fi
 }
 
 # Unistall a package
-nixunistall()
+function nixuninstall()
 {
-	nix-env -e "$1";
+	if nix-env -e "$1" &&  [ -f ~/.config/nix_packages.yaml ]; then
+		sed -i "/^-\s$1$/d" ~/.config/nix_packages.yaml;
+	fi
 }
 
 # Clean unistalled packages
-nixclean()
+function nixclean()
 {
 	nix-env --deleting-generations old;
 	nix-store --gc;
 }
 
 # Update packages
-nixupdate()
+function nixupdate()
 {
 	echo "Updating channels"
 	nix-channel --update;
